973cdeff1fbee36c90fba364a635b339cbe6ad2b
evaldojuniorkj <evaldojunior.kj@gmail.com>
2021-12-27 18:35:16 -0400

    Fundamentos 1 Concluido
    terminando o modulo de Fundamentos 1 React direto ao ponto.

--- reagindo/contador/src/index.js
+++ reagindo/contador/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Numero } from './numeros/Numero';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />,
+    <Numero/>
   </React.StrictMode>,
   document.getElementById('root')
 );
